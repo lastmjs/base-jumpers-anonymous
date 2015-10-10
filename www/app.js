f5b7fc494ad3f50a6dc4c70aff6aac48b92038ee
Jordan Last <jordan.michael.last@gmail.com>
2015-10-10 12:50:39 -0600

    original app

--- www/app.js
+++ www/app.js
@@ -1,70 +1,50 @@
 angular.module('BJA', [])
-.run(function() {
-	persistence.store.websql.config(persistence, 'bja', 'the Base Jumpers Anonymous WebSQL database', 5 * 1024 * 1024);
-	persistence.schemaSync();
-})
 .controller('MainController', ['$scope',
 	function($scope) {
 		$scope.showSignUpArea = true;
 	}
 ])
-.controller('SignUpController', ['$scope', 'User',
-	function($scope, User) {
+.controller('SignUpController', ['$scope',
+	function($scope) {
 		
 		$scope.signUp = function(firstName, lastName, username, email) {
 			
-			var user = new User();
-			
-			user.firstName = firstName;
-			user.lastName = lastName;
-			user.username= username;
-			user.email = email;
-			
-			user.save(function() {
-				alert('User saved!');
-			});
+			alert('User saved!');
 			
 		};
 		
 	}
 ])
-.controller('ViewJumpsController', ['$scope', 'Jump',
-	function($scope, Jump) {
+.controller('ViewJumpsController', ['$scope',
+	function($scope) {
 		
 		$scope.showJumps = function(username) {
 			
-			Jump.getAllByUsername(username, function(jumps) {
-				$scope.$apply(function() {
-					$scope.jumps = jumps;
-				});
-			});
-			
-		};
-		
-		$scope.deleteJump = function(jump) {
-			
-			jump.executeDelete(function() {
-				$scope.showJumps(jump.username);
-			});
+			$scope.jumps = [
+				{
+					title: 'Half Dome',
+					description: 'I hit the side of the rock!'
+				},
+				{
+					title: 'San Francisco',
+					description: 'Lol, my chute almost didn\'t come out'
+				},
+				{
+					title: 'Eiffel Tower',
+					description: 'It was fun'
+				}
+			];
 			
 		};
 		
 	}
 ])
-.controller('CreateJumpController', ['$scope', 'Jump',
-	function($scope, Jump) {
+.controller('CreateJumpController', ['$scope',
+	function($scope) {
 		
 		$scope.createJump = function(title, description, username) {
 			
-			var jump = new Jump();
-			
-			jump.title = title;
-			jump.description = description;
-			jump.username = username;
-			
-			jump.save(function() {
-				alert('Jump saved!');
-			});
+			alert('Jump saved!');
 			
 		};
 		
