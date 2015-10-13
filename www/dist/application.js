5bfbdaf443556a645e116c1c7f551ae8a912fad8
Jordan Last <jordan.michael.last@gmail.com>
2015-10-12 21:20:01 -0600

    final app with live reload

--- www/dist/application.js
+++ www/dist/application.js
@@ -1,131 +1,52 @@
-angular.module('BJA', [])
-.run(function() {
-	persistence.store.websql.config(persistence, 'bja', 'the Base Jumpers Anonymous WebSQL database', 5 * 1024 * 1024);
-	persistence.schemaSync();
-})
+angular.module('BJA', []) 
 .controller('MainController', ['$scope',
 	function($scope) {
 		$scope.showSignUpArea = true;
 	}
 ])
-.controller('SignUpController', ['$scope', 'User',
-	function($scope, User) {
+.controller('SignUpController', ['$scope',
+	function($scope) {
 		
 		$scope.signUp = function(firstName, lastName, username, email) {
 			
-			var user = new User();
-			
-			user.firstName = firstName;
-			user.lastName = lastName;
-			user.username= username;
-			user.email = email;
-			
-			user.save(function() {
-				alert('User saved!');
-			});
+			alert('User saved!');
 			
 		};
 		
 	}
 ])
-.controller('ViewJumpsController', ['$scope', 'Jump',
-	function($scope, Jump) {
+.controller('ViewJumpsController', ['$scope',
+	function($scope) {
 		
 		$scope.showJumps = function(username) {
 			
-			Jump.getAllByUsername(username, function(jumps) {
-				$scope.$apply(function() {
-					$scope.jumps = jumps;
-				});
-			});
-			
-		};
-		
-		$scope.deleteJump = function(jump) {
-			
-			jump.executeDelete(function() {
-				$scope.showJumps(jump.username);
-			});
+			$scope.jumps = [
+				{
+					title: 'Half Dome',
+					description: 'I hit the side of the rock!'
+				},
+				{
+					title: 'San Francisco',
+					description: 'Lol, my chute almost didn\'t come out'
+				},
+				{
+					title: 'Eiffel Tower',
+					description: 'It was fun'
+				}
+			];
 			
 		};
 		
 	}
 ])
-.controller('CreateJumpController', ['$scope', 'Jump',
-	function($scope, Jump) {
+.controller('CreateJumpController', ['$scope',
+	function($scope) {
 		
 		$scope.createJump = function(title, description, username) {
 			
-			var jump = new Jump();
-			
-			jump.title = title;
-			jump.description = description;
-			jump.username = username;
-			
-			jump.save(function() {
-				alert('Jump saved!');
-			});
+			alert('Jump saved!');
 			
 		};
 		
-	}
-]);
-angular.module('BJA').factory('Jump', [
-	function() {
-		
-		var Jump = persistence.define('Jump', {
-			title: 'TEXT',
-			description: 'TEXT',
-			username: 'TEXT',
-			deletedAt: 'DATE'
-		});
-		
-		Jump.prototype.save = function(callback) {
-		
-			persistence.add(this);
-			persistence.flush(function() {
-				callback && callback();
-			});
-			
-		};
-		
-		Jump.prototype.executeDelete = function(callback) {
-			
-			this.deletedAt = new Date();
-			this.save(callback);
-			
-		};
-		
-		Jump.getAllByUsername = function(username, callback) {
-			
-			Jump.all().filter('username', '=', username).and(new persistence.PropertyFilter('deletedAt', '=', null)).list(function(jumps) {
-				callback(jumps);
-			});
-			
-		};
-		
-		return Jump;
-	}
-]);
-angular.module('BJA').factory('User', [
-	function() {
-		
-		var User = persistence.define('User', {
-			firstName: 'TEXT',
-			lastName: 'TEXT',
-			username: 'TEXT',
-			email: 'TEXT'
-		});
-		
-		User.prototype.save = function(callback) {
-		
-			persistence.add(this);
-			persistence.flush(function() {
-				callback && callback();
-			});
-			
-		};
-		
-		return User;
 	}
 ]);
